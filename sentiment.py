33d304672d35a7d0dfaa5155c76c2c9f35c9ed87
Ross Smith <RossSmith0603@gmail.com>
2026-06-16 16:13:04 +0100

    Before processing we need to deal with stopwords. Created a set containing all of the stopwords

--- sentiment.py
+++ sentiment.py
@@ -1,13 +1,19 @@
 import nltk 
 from nltk.corpus import movie_reviews
+from nltk.corpus import stopwords
 
 nltk.download('movie_reviews')
+nltk.download('stopwords')
 
 # Load reviews and labels
 
 reviews = [(movie_reviews.raw(fileid), category)
            for category in movie_reviews.categories()
            for fileid in movie_reviews.fileids(category)]
+
+stop_words = set(stopwords.words('english'))
+
+print(list(stop_words)[:20])
 
 print(f"Total reviews: {len(reviews)}")
 print(f"Categories: {movie_reviews.categories()}")

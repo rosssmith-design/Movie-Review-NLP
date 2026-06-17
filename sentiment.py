0ed0ce6179c40025119166ff0e6c76450266a6a4
Ross Smith <RossSmith0603@gmail.com>
2026-06-17 18:47:14 +0100

    New best test acc at 86.65%. Same model as before using a larger dataset, specifically the IMDB reviews dataset

--- sentiment.py
+++ sentiment.py
@@ -1,23 +1,25 @@
-import nltk 
-from nltk.corpus import movie_reviews
-from nltk.corpus import stopwords
 import re
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+from datasets import load_dataset
+dataset = load_dataset('stanfordnlp/imdb')
 
-nltk.download('movie_reviews')
-nltk.download('stopwords')
+print(dataset)
+print(dataset['train'][0])
+
 
 # Load reviews and labels
 
-reviews = [(movie_reviews.raw(fileid), category)
-           for category in movie_reviews.categories()
-           for fileid in movie_reviews.fileids(category)]
+train_texts = [item['text'] for item in dataset['train']]
+train_labels = [item['label'] for item in dataset['train']]
+test_texts = [item['text'] for item in dataset['test']]
+test_labels = [item['label'] for item in dataset['test']]
 
 # All english stop words
-stop_words = set(stopwords.words('english'))
+stop_words = set(ENGLISH_STOP_WORDS)
 
 # Preprocessing function for training
 def preprocess(text):
@@ -31,28 +33,26 @@ def preprocess(text):
     return ' '.join(words)
 
 # Applying preprocessing to all reviews
-texts = [preprocess(review[0]) for review in reviews]
-labels = [review[1] for review in reviews]
+train_texts_clean = [preprocess(text) for text in train_texts]
+test_texts_clean = [preprocess(text) for text in test_texts]
 
 # Vectorizing the preprocessed text
 vectorizer = CountVectorizer(max_features=5000)
-X = vectorizer.fit_transform(texts)
+X_train = vectorizer.fit_transform(train_texts_clean)
+X_test = vectorizer.transform(test_texts_clean)
 
-# Spliting the data
-X_train, X_test, y_train, y_test = train_test_split(X, labels, test_size=0.2, random_state=42)
 
 # Train model
 model = LogisticRegression(max_iter=1000, C=0.1)
-model.fit(X_train, y_train)
+model.fit(X_train, train_labels)
 
 # Evaluate
 train_pred = model.predict(X_train)
 test_pred = model.predict(X_test)
 
-print(f"Total preprocessed reviews: {len(texts)}")
-print(f"Labels: {labels.count('neg')} negative, {labels.count('pos')} positive")
-print(f"Matrix shape: {X.shape}")
-print(f"Training Accuracy: {accuracy_score(y_train, train_pred) * 100:.2f}%")
-print(f"Test Accuracy: {accuracy_score(y_test, test_pred) * 100:.2f}%")
+print(f"Total reviews: {len(dataset)}")
+
+print(f"Training Accuracy: {accuracy_score(train_labels, train_pred) * 100:.2f}%")
+print(f"Test Accuracy: {accuracy_score(test_labels, test_pred) * 100:.2f}%")
 
 

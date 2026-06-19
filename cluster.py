d443b6587ada04c56dff362e1f379be216652b95
Ross Smith <RossSmith0603@gmail.com>
2026-06-19 20:17:05 +0100

    Attempting to reduce spatial dimension for clustering however now variance explained is at only 33.08%

--- cluster.py
+++ cluster.py
@@ -1,6 +1,9 @@
 from datasets import load_dataset
 import re
 from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
+import joblib
+from sklearn.decomposition import PCA
+
 
 
 # All english stop words
@@ -21,5 +24,22 @@ def preprocess(text):
 dataset = load_dataset('stanfordnlp/imdb')
 unsupervised_texts = [item['text'] for item in dataset['unsupervised']]
 
+# Load saved model and vectorizer
+vectorizer = joblib.load('sentiment_vectorizer.pkl')
+model = joblib.load('sentiment_model.pkl')
+
+# Preprocess the unsupervised reviews
+unsupervised_clean = [preprocess(text) for text in unsupervised_texts]
+
+# Vectorize unsupervised dataset
+X_unsupervised = vectorizer.transform(unsupervised_clean)
+
+# Reduce to 100 dimensions 
+pca = PCA(n_components=100, random_state=42)
+X_reduced = pca.fit_transform(X_unsupervised.toarray())
+
+
 print(f"Total unsupervised reviews: {len(unsupervised_texts)}")
-print(unsupervised_texts[0][:300])
+print(f"Matrix shape: {X_unsupervised.shape}")
+print(f"Reduced shape: {X_reduced.shape}")
+print(f"Variance explained: {pca.explained_variance_ratio_.sum()*100:.2f}%")
